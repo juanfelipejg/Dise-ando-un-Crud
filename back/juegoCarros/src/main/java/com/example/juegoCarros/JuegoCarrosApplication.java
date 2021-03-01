94d4c0d3711f4ddae9bde3b238103acfe0c82fcb
jjaramga <juanfelipejg@gmail.com>
2021-03-01 16:12:02 -0500

    Insertion Initializr

---
+++ back/juegoCarros/src/main/java/com/example/juegoCarros/JuegoCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.juegoCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoCarrosApplication.class, args);
+	}
+
+}
