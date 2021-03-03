8d13bd3a7a028538b962b2da3c73ae579f4aa369
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-03 16:05:38 -0500

    Change - CreatePlayerView

--- front/car-game/src/views/CreatePlayerView.js
+++ front/car-game/src/views/CreatePlayerView.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CreatePlayerView = () =>{
 
-    const startGame = () => {
-        
+    const [players, setPlayers] = useState();
+
+    const startGame = (event) => {
+        event.preventDefault();
+
+        console.log(players);
     };
+    
+    const submitForm = (event) =>{
+        event.preventDefault();
+
+        setPlayers({...players, [event.target.name]: event.target.value})
+    }
 
     return(
         <div className="container p-3 form-container">
-            <form>
+            <form onSubmit = {startGame}>
                 <div className="mb-3">
                     <label className="form-label">Jugador 1</label>
-                    <input type="text" className="form-control" id="players"></input>
+                    <input type="text" className="form-control" name = "player1" onChange={submitForm}></input>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Jugador 2</label>
-                    <input type="text" className="form-control" id="distance"></input>
+                    <input type="text" className="form-control" name = "player2" onChange={submitForm}></input>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Jugador 3</label>
-                    <input type="text" className="form-control" id="distance"></input>
+                    <input type="text" className="form-control" name = "player3" onChange={submitForm}></input>
                 </div>
                 <div className = "button-container">
-                    <button className="btn btn-primary" onClick = {startGame}>Empezar Juego</button>
+                    <button type = "submit" className="btn btn-primary">Empezar Juego</button>
                 </div>
             </form>
         </div>
