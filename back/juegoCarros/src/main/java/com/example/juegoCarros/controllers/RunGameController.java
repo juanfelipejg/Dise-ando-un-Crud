535eccb446474753823a4404cdcd00b449487a42
danielburgosr96 <danielburgosr96@gmail.com>
2021-03-04 14:04:57 -0500

    Edit - Result Repository

---
+++ back/juegoCarros/src/main/java/com/example/juegoCarros/controllers/RunGameController.java
@@ -0,0 +1,2 @@
+package com.example.juegoCarros.controllers;public class RunGameController {
+}
--- back/juegoCarros/src/main/java/com/example/juegoCarros/repositories/ResultRepository.java
+++ back/juegoCarros/src/main/java/com/example/juegoCarros/repositories/ResultRepository.java
@@ -1,8 +1,10 @@
 package com.example.juegoCarros.repositories;
 
+import com.example.juegoCarros.entities.PartialResult;
+import com.example.juegoCarros.entities.Player;
 import com.example.juegoCarros.entities.Result;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ResultRepository extends CrudRepository<Result, Integer> {
-
+    public Result findByPlayer(Player player);
 }
